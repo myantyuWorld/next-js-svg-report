0ef9edb6f493b8af95e9a91c8491f290d65a425c
yuichi.oba <yuichi.oba@sorich.jp>
2023-06-27 14:50:01 +0900

    svgファイルで表現した帳票をnext.jsで表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import dynamic from 'next/dynamic'
-import Report1 from '../components/Report1'
+// import Report1 from '../components/Report1'
 
 import styles from '../styles/Home.module.css'
 
 // ================
 // tips
 // ================
-// dynamic importは不要
-// ただし、DOM描画「後」でないと、svg pager内の処理で、DOM取得に失敗する。
-// ので、useEffectで描画した後に、書き換え処理を行うようにする
-// 
-// const Report = dynamic(()  => import('../components/Report1'), {
-//   ssr : false,
-// })
+const Report1 = dynamic(()  => import('../components/Report1'), {
+  ssr : false,
+})
 
 export default function Home() {
   return (
